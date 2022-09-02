2fc92e208f227cb598e8193428334d4f8eba203d
aleinin <aleininger.cs@gmail.com>
2022-09-02 00:40:53 -0500

    refactoring + additional title

--- roll_parser.py
+++ roll_parser.py
@@ -26,32 +26,55 @@ class RollParser:
         else:
             return -1, -1
 
+    # increments or initializes a roll count
+    @staticmethod
+    def add_roll_to_dict(dictionary, key):
+        if key not in dictionary:
+            dictionary[key] = 1
+        else:
+            dictionary[key] += 1
+
+    # extract the title from an inlineroll
+    # attempts multiple possible keys based on
+    # roll20 title names
+    @staticmethod
+    def extract_title(roll):
+        try:
+            return roll['title']
+        except KeyError:
+            pass
+        try:
+            return roll['original-title']
+        except KeyError:
+            return None
+
     # given an inlineroll, returns what quantity, kind of dice,
     # and what number was rolled.
     # ex 1d20 rolled an 8
     @staticmethod
     def get_roll_info(roll):
-        title = roll['title']
+        title = RollParser.extract_title(roll)
+        if title is None:
+            return -1, -1, -1
         number_of_dice, type_of_dice = RollParser.get_type_of_dice(title)
         num_rolled_extract = re.search('>[0-9][0-9]?<', title)
         if num_rolled_extract and number_of_dice != -1:
             number_rolled = int(num_rolled_extract.group(0).replace(">", "").replace("<", ""))
+            return number_of_dice, type_of_dice, number_rolled
         else:
             return -1, -1, -1
-        return number_of_dice, type_of_dice, number_rolled
 
-    # Figures out the date the roll is from by parsing the time stamp
     def get_session(self, msg):
         parsed_date_line = msg.find("span", class_="tstamp")
         date = ""
         if parsed_date_line is not None:
             parsed_date = parsed_date_line.contents[0]
             date = re.sub(' [0-9]*:[0-9]*[A|P]M', "", parsed_date)
-        if date not in self.debug_dates and date != "":
-            self.debug_dates[date] = 1
-        elif date != "":
-            self.debug_dates[date] = self.debug_dates[date] + 1
         if date != "":
+            if date not in self.debug_dates:
+                self.debug_dates[date] = 1
+            else:
+                self.debug_dates[date] += 1
             self.session = date
 
     # returns if the roll is from the date supplied
@@ -69,6 +92,16 @@ class RollParser:
                 self.recent_sender = self.recent_sender[1:-1].replace("From ", "")
             self.debug_set.add(self.recent_sender)
 
+    # record roll to player, initializing data if necessary
+    def add_roll_to_player(self, number_rolled):
+        if self.recent_sender in self.senders:
+            current_rolls = self.senders[self.recent_sender]
+        else:
+            # todo non d20 support
+            current_rolls = [0] * 20
+        current_rolls[int(number_rolled) - 1] += 1
+        self.senders[self.recent_sender] = current_rolls
+
     # main function that parses each message in the chat log
     # it then finds the attack cards inside those messages
     # and gathers data about the roll and records it along
@@ -85,21 +118,12 @@ class RollParser:
                     attacks = roll_cards.find_all("span", class_=re.compile(r'inlinerollresult.*'))
                     for roll in attacks:
                         number_of_dice, type_of_dice, number_rolled = RollParser.get_roll_info(roll)
-                        if self.debug:
-                            print("{} rolled a {}d{} for {} on {}".format(self.recent_sender, number_of_dice,
-                                                                          type_of_dice, number_rolled, self.session))
                         if type_of_dice == die_to_record:
-                            if self.recent_sender in self.senders:
-                                current_rolls = self.senders[self.recent_sender]
-                                current_rolls[int(number_rolled) - 1] += 1
-                                self.senders[self.recent_sender] = current_rolls
-                                # otherwise create a new entry
-                            else:
-                                current_rolls = []
-                                for i in range(1, 21):
-                                    current_rolls.append(0)
-                                current_rolls[int(number_rolled) - 1] += 1
-                                self.senders[self.recent_sender] = current_rolls
+                            if self.debug:
+                                print("{} rolled a {}d{} for {} on {} (#{})".format(self.recent_sender, number_of_dice,
+                                                                                    type_of_dice, number_rolled,
+                                                                                    self.session, msg_num))
+                            self.add_roll_to_player(number_rolled)
         if self.debug:
             self.debug_print()
         return self.senders
